aa5fdb9e7200fe9083ecdc875283b3383429724b
FelipeIE <felipecorpiefe@Gmail.com>
2025-02-26 17:54:02 -0300

    Constructing Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Funcionatily } from './pages/Functionality/Functionality';
 import { Navbar } from './pages/Components/Navbar/Navbar';
+import {Footer} from './pages/Components/Footer/Footer';
+
 
 export const App = () => {
   return (
@@ -10,6 +12,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Funcionatily />} />
       </Routes>
+      <Footer/>
     </Router>
 
   )
