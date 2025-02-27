880006decb60df00d7f793fe1b3cc87c4f389c3c
FelipeIE <felipecorpiefe@Gmail.com>
2025-02-27 17:03:33 -0300

    Created buttons of the control card

--- src/pages/Components/Card/Card.jsx
+++ src/pages/Components/Card/Card.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../Card/Card.scss';
+import IconLixeira from './assets/IconLixeira.png'
+import IconLapis from './assets/IconLapis.png'
 
 export const Card = () => {
   return (
     <div className="CardContainer">
-        <div className="TitiulodoCard">
-            <input placeholder='Titulo Do Card'/>
-        </div>
-        <textarea name="" id=""></textarea>
-        <button>COPIAR</button>
+      <div className="TitiulodoCard">
+        <button id='BtnDeleteCard'><img src={IconLixeira} alt="Botão Deletar Card" /></button>
+        <input placeholder='Titulo Do Card' />
+        <button id='BtnEditCard'><img src={IconLapis} alt="Botão Editar Card" /></button>
+      </div>
+      <textarea name="" id=""></textarea> <button id='BtnEditTextArea'><img src={IconLixeira} alt="Botão Deletar Card" /></button>
+      <button>COPIAR</button>
     </div>
   )
 }
